3033e699c4cbc3137e4bab6f3833aad85a1678bb
Julius van Mansvelt <juliusvanmansvelt@Juliuss-MacBook-Pro.local>
2018-05-10 15:10:09 +0200

    Fixed

--- Websocket.js
+++ Websocket.js
@@ -6,6 +6,8 @@ module.exports.createWebsocket = function(server)
   
     const wss = new WebSocket.Server({ server });
 
+   
+
     wss.on('connection', function connection(ws, req) {
     
       var urlParam = req.url.replace('/', '');
@@ -26,34 +28,15 @@ module.exports.createWebsocket = function(server)
       });
     
       ws.on('close', function close() {
-        ws.send("Disconnected")
-        console.log("Disconnected")
+        console.log("Client disconnected")
         clearInterval(pingClient)
       });
     
       var pingClient = setInterval(function() {
-       
-        try {
-          ws.send("Ping", function(error) {
-            if(error == undefined)
-              return;
-            else
-              logger.debug("Async error:"+error);
-          });
-        } catch(e) {
-          logger.debug("Sync error: " + e);
-            ws.close();
-        }
-
-
-      //   ws.clients.forEach(function each(client) {
-          
-      //     if (client.readyState === client.OPEN)
-      //     {
-      //       ws.send("ping")
-      //     }
-      // });
-
+      
+        
+        ws.send("Ping")
+        console.log("Pinged client")
           
       }, interval);
 
