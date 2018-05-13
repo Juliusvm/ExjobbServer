221bed45c09cefb3fc8ac951cfc1d920bae7010c
Jullix993 <Julius.van.mansvelt@gmail.com>
2018-05-13 14:49:04 +0200

    ea

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function makeid() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 4; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
